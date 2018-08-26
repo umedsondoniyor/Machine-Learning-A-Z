f4163167eedde6f45db5663295eafb18037b76df
umedzhonizbasarov@gmail.com <umedzhonizbasarov@gmail.com>
2018-08-26 17:57:38 +0300

    Splitting to test & train done

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -10,11 +10,14 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values # independent inp values with 
 y = dataset.iloc[:, 3].values   # dependent outp value 
 
+
 # Taking care of missing data which is quite common
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3]) # transform func replaces the missing data
+
+
 
 # Encoding categorical data
 # we change alphabetical data to numerical
@@ -30,9 +33,12 @@ labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
 
 
 # Feature Scaling
