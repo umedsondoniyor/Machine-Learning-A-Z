b5de5b070a561ff3b91f075d857adf3b492dc3b6
umedzhonizbasarov@gmail.com <umedzhonizbasarov@gmail.com>
2018-08-26 17:29:03 +0300

    missing data and categorical encoding is done

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -7,12 +7,33 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values # independent inp values with 
+y = dataset.iloc[:, 3].values   # dependent outp value 
+
+# Taking care of missing data which is quite common
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3]) # transform func replaces the missing data
+
+# Encoding categorical data
+# we change alphabetical data to numerical
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# we do dummy encoding here with OneHotEncoder function in order to
+# give equal weights to categories which must have equal weights
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+#  we encoded dependent output data also we convert yes-no to 1-0
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
